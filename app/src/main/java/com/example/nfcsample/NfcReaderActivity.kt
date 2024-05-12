c405b50116d022ce30393f476a50a89fdec66084
Ehsan Haghdoust <ehsan.haghdoust@gmail.com>
2024-05-13 02:00:51 +0330

    - Changed some code

--- app/src/main/java/com/example/nfcsample/NfcReaderActivity.kt
+++ app/src/main/java/com/example/nfcsample/NfcReaderActivity.kt
@@ -1,30 +1,82 @@
 package com.example.nfcsample
 
-import android.content.Intent
 import android.nfc.NdefMessage
+import android.nfc.NdefRecord
 import android.nfc.NfcAdapter
+import android.nfc.Tag
+import android.nfc.tech.Ndef
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.nfcsample.databinding.ActivityNfcReaderBinding
+import java.io.IOException
 
-class NfcReaderActivity: AppCompatActivity() {
+class NfcReaderActivity : AppCompatActivity(), NfcAdapter.ReaderCallback {
+
+    private lateinit var binding: ActivityNfcReaderBinding
+
+    private lateinit var mNfcAdapter: NfcAdapter
+    private lateinit var mUrl: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityNfcReaderBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this)
+        if (!mNfcAdapter.isEnabled) {
+            Toast.makeText(this, "Activate NFC feature", Toast.LENGTH_LONG).show()
+            finish()
+        }
     }
 
+    override fun onTagDiscovered(tag: Tag) {
 
-    override fun onNewIntent(intent: Intent) {
-        super.onNewIntent(intent)
-        if (NfcAdapter.ACTION_NDEF_DISCOVERED == intent.action) {
-            intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)?.also { rawMessages ->
-                val messages: List<NdefMessage> = rawMessages.map { it as NdefMessage }
-                // Process the messages array.
-                val messageResp = messages.toString()
-                Toast.makeText(this, messageResp, Toast.LENGTH_LONG).show()
+        val mNdef = Ndef.get(tag);
+        if (mNdef != null) {
+            val mRecord = NdefRecord.createUri(mUrl);
+            val mNdefMsg = NdefMessage(mRecord)
+            try {
+                mNdef.connect();
+                mNdef.writeNdefMessage(mNdefMsg)
+                runOnUiThread {
+                    Toast.makeText(this, "NFC tag detected", Toast.LENGTH_SHORT).show()
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            } finally {
+                try {
+                    mNdef.close();
+                } catch (e: IOException) {
+                    Toast.makeText(this, "IO error happened", Toast.LENGTH_SHORT).show()
+                }
+            }
+        } else {
+            runOnUiThread {
+                Toast.makeText(this, "Invalid tag", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
-      // Check overall gas and return result
+    override fun onResume() {
+        super.onResume()
+        val options = Bundle()
+        options.putInt(NfcAdapter.EXTRA_READER_PRESENCE_CHECK_DELAY, 1024)
+        mNfcAdapter.enableReaderMode(
+            this,
+            this,
+            NfcAdapter.FLAG_READER_NFC_A or
+                    NfcAdapter.FLAG_READER_NFC_B or
+                    NfcAdapter.FLAG_READER_NFC_F or
+                    NfcAdapter.FLAG_READER_NFC_V or
+                    NfcAdapter.FLAG_READER_NFC_BARCODE,
+            options
+        )
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mNfcAdapter.disableReaderMode(this);
+    }
 }
