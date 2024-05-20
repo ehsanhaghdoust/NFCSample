994ceefd53f4f823a4c02335598630603b95b5ce
Ehsan Haghdoust <ehsan.haghdoust@gmail.com>
2024-05-20 12:30:27 +0330

    .

---
+++ app/src/main/java/com/example/nfcsample/view/cardSimulation/CardSimulationActivity.kt
@@ -0,0 +1,13 @@
+package com.example.nfcsample.view.cardSimulation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CardSimulationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+//    override fun onTr
+}
